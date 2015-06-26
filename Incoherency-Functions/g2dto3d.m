a550023efcaac239031fbb3b9c19f72e1acad6aa
Christian Reinicke <culeche@gmail.com>
2015-06-26 12:16:26 +0200

    Modified g2dto3d.m

--- Incoherency-Functions/g2dto3d.m
+++ Incoherency-Functions/g2dto3d.m
@@ -1,13 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PURPOSE 
-% * Transform 2d gamma matrix to 3d gamma matrix
+% * Transform 2d blending matrix to 3d blending matrix
 
 % INPUT
-% * g2d:    2d gamma matrix: Soure x Experiment
+% * g2d:    2d blending matrix: Soure x Experiment
 % * t_g:    Shooting window
 
 % OUTPUT
-% * g3d:    3d gamma matrix: Soure x Experiment x Time Ns x Ne x t_g
+% * g3d:    3d blending matrix: Soure x Experiment x Time Ns x Ne x t_g
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -20,7 +20,6 @@ function g3d = g2dto3d(g2d,t_g)
 if norm(round(g2d) - g2d) ~= 0
     error('g2dto3d.m expects a blending matrix g2d with integer entries.');
 end
-
 
 g3d = zeros(Ns,Ne,t_g);
 
