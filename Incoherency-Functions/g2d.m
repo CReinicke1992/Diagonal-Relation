3498591ee5e17792e099393f893931a982ae4b54
Christian Reinicke <culeche@gmail.com>
2015-06-26 12:00:41 +0200

    Modified g2d.m to make things clearer

--- Incoherency-Functions/g2d.m
+++ Incoherency-Functions/g2d.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PURPOSE
-% * This script only blends sources of a 2D source grid
+% * Create a 2d blending matrix
 
 % INPUT PARAMETERS
 % * t_g   Blending window.
@@ -18,14 +18,20 @@ function [g,Ne] = g2d(t_g,b,Ns,pattern)
 
 
 % Number of experiments, initialize g
-Ne = round(Ns/b);   g = zeros(Ns,Ne);
+Ne = Ns/b;   g = zeros(Ns,Ne);
+
+% Ne must be an integer
+if Ne ~= round(Ne)
+    error('The number of sources Ns must be a multiple of the blending factor b.')
+end
 
 % Set default blending pattern
 if nargin < 4
-    pattern = 1;
+    pattern = 0;
 end
 
-% Pattern 0: b ajacent sources are blended with random time delays in
+% Pattern 0: Time
+% b ajacent sources are blended with random time delays in
 % order, eg from the left to the right
 if pattern == 0
     
@@ -33,7 +39,8 @@ if pattern == 0
         g( 1+(exp-1)*b : exp*b,exp ) = [1 ; t_g * sort( rand(b-1,1) ) ];
     end
     
-% Pattern 1: b adjacent sources are blended with random time delays in
+% Pattern 1: Time-Space-Experiment
+% b adjacent sources are blended with random time delays in
 % random order
 elseif pattern == 1
     
@@ -41,7 +48,8 @@ elseif pattern == 1
         g( 1+(exp-1)*b : exp*b,exp ) = [1 ; t_g*rand(b-1,1) ];
     end
     
-% Pattern 2: b randomly picked sources are blended with random time delays    
+% Pattern 2: Time-Space
+% b randomly picked sources are blended with random time delays    
 elseif pattern == 2
     
     for exp = 1:Ne
@@ -52,7 +60,8 @@ elseif pattern == 2
     ind = randperm(Ns);
     g(ind,:) = g;
     
-% Pattern 3: b randomly picked sources are blended with no time delay
+% Pattern 3: Space
+% b randomly picked sources are blended with no time delay
 elseif pattern == 3
     
     for exp = 1:Ne
@@ -63,7 +72,8 @@ elseif pattern == 3
     ind = randperm(Ns);
     g(ind,:) = g;
     
-% Pattern 4: b adjacent sources are blended with no time delay in
+% Pattern 4: None
+% b adjacent sources are blended with no time delay in
 % order, eg from the left to the right
 elseif pattern == 4
     
