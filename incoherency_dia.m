c2e2499de2dd435bea74c08e0ecea12a3a0007fc
Christian Reinicke <culeche@gmail.com>
2015-06-27 12:49:17 +0200

    New file to compute incoherency

---
+++ incoherency_dia.m
@@ -0,0 +1,45 @@
+% Required m-files:
+%   * crane.m
+%   * g3d.m
+%   * g3dto2d.m
+
+
+function in = incoherency_dia(Ns,Nt,b,tg,pattern)
+
+[G3,~] = crane(Ns,Nt,b,tg,pattern);
+
+%% Compute GGH, sum along diagonals, and sum over all frequency components
+
+% Initialize matrix to save the sums along diagonals for each frequency
+% component separately
+diagsum = zeros(2*Ns-1,Nt);
+
+% Iterate over all frequency components
+for w = 1:size(G3,3)
+    
+    % Compute G*Gh
+    G = squeeze( G3(:,:,w) );
+    Gh = G';
+    GGH = G*Gh;
+    
+    % Sum along diagonals
+    % Save the result in diagsum
+    for dia = 1-Ns:Ns-1
+        diagsum(dia+Ns,w) =  abs( sum(diag(GGH,dia)) );
+    end
+        
+    inco(1,w) = diagsum(Ns,w)^2 / sum(diagsum(:,w).^2);
+   
+end
+
+% Sum over all frequency components
+% Ideally the output is the autocorrelation with respect to source lag
+autocorr = sum(diagsum,2);
+
+figure(1); plot( autocorr(:,1) ); title('autocorrelation for summed frequencies');
+figure(2); plot( diagsum(:,30) ); title('autocorrelation of w = 30');
+figure(3); plot( inco); xlabel('Frequency')
+
+in = autocorr(Ns,1)^2 / sum(autocorr.^2);
+%in = 10*log10( autocorr(1,1)^2 / sum(autocorr.^2) );
+%in_mod = mean(inco);
