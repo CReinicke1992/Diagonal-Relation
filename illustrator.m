6878eae347366180fbe9a125a2370a8bdffabb2e
Christian Reinicke <culeche@gmail.com>
2015-06-27 12:47:46 +0200

    Included all subdiagonals in incoherency calculation

--- illustrator.m
+++ illustrator.m
@@ -12,7 +12,7 @@ b   = 5;        % Blending factor
 dt  = 0.002;    % Sampling rate: Seconds per sample
 tg  = 100;      % Maximum time delay in time samples
 Nt  = 201;      % Number of time samples
-pattern = 0;    % Blending pattern (Time + Space)
+pattern = 4;    % Blending pattern (Time + Space)
 
 % Patterns:
 % 0     Time
@@ -27,7 +27,7 @@ pattern = 0;    % Blending pattern (Time + Space)
 
 % Initialize matrix to save the sums along diagonals for each frequency
 % component separately
-diagsum = zeros(Ns,Nt);
+diagsum = zeros(2*Ns-1,Nt);
 inco = zeros(1,Nt);
 
 % Iterate over all frequency components
@@ -40,11 +40,11 @@ for w = 1:size(G3,3)
     
     % Sum along diagonals
     % Save the result in diagsum
-    for dia = 1:Ns
-        diagsum(dia,w) =  abs( sum(diag(GGH,dia-1)) );
+    for dia = 1-Ns:Ns-1
+        diagsum(dia+Ns,w) =  abs( sum(diag(GGH,dia)) );
     end
-    
-    inco(1,w) = diagsum(1,w)^2 / sum(diagsum(:,w).^2);
+        
+    inco(1,w) = diagsum(Ns,w)^2 / sum(diagsum(:,w).^2);
    
 end
 
@@ -56,6 +56,6 @@ figure(1); plot( autocorr(:,1) ); title('autocorrelation for summed frequencies'
 figure(2); plot( diagsum(:,30) ); title('autocorrelation of w = 30');
 figure(3); plot( inco); xlabel('Frequency')
 
-in = autocorr(1,1)^2 / sum(autocorr.^2);
+in = autocorr(Ns,1)^2 / sum(autocorr.^2);
 %in = 10*log10( autocorr(1,1)^2 / sum(autocorr.^2) );
 %in_mod = mean(inco);
